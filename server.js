69697d22b54cfbefd7deb4a8eeaa1e4ed458ac24
[Chris Becker} <oneringtobeckerthem@gmail.com>
2022-07-31 20:24:32 -0500

    finished server midware and installs

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 
-mongoose.connect(process.env.DB_CONNECTION)
+mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true}, () => {console.log('Connected to database')})
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
+
